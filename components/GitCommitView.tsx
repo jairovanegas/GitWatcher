64500f7683171d95ddbb2a5296bd225972ebc831
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-10-27 09:03:30 -0500

    Github User api query implemented

--- components/GitCommitView.tsx
+++ components/GitCommitView.tsx
@@ -1,12 +1,33 @@
-import { GithubCommit } from "../pages";
+import { GithubCommit, GithubUser } from "../pages";
 import Image from 'next/image'
+import { useEffect, useState } from "react";
+import { Octokit } from "octokit";
 
 export default function GitCommitView({ commit }: { commit: GithubCommit }) {
+
+    const [userInfo, setUserInfo] = useState(commit.author);
+    
+    useEffect(() => {
+        if (userInfo.avatar_url === "") {
+            const octokit = new Octokit({
+                auth: process.env["ACCESS_TOKEN"]
+            });
+            octokit.request('GET /users/{username}', {
+                username: userInfo.login
+            }).then((value) => {
+                const user: GithubUser = value.data;
+                setUserInfo((previousUser) => {
+                    return user;
+                });
+            })
+        }
+    }, []);
+
     return (
         <div className="shadow-md content-center items-center mt-3 mb-3 bg-white rounded-md p-2">
             <p className="text-3xl text-justify text-black">Commit message: <a href={commit.html_url} target="_blank" className="hover:text-blue-700">{commit.commit.message}</a></p>
             <div className="flex items-center text-black">
-                <p className="text-left text-2xl mr-2">Author:</p> 
+                <p className="text-left text-2xl mr-2">Author:</p>
                 <div className="h-10 w-10 bg-cover rounded-md" style={{ backgroundImage: `url(${commit.author.avatar_url})` }} />
                 <div className="text-left text-2xl ml-2">
                     <p><a href={commit.author.html_url} target="_blank" className="hover:text-blue-700">{commit.author.login}</a></p>
