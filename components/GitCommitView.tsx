608d71ffbc1c5c34fd46daa993af84969deb8fc6
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-10-27 16:09:31 -0500

    Anchor missing property fixed

--- components/GitCommitView.tsx
+++ components/GitCommitView.tsx
@@ -30,12 +30,12 @@ export default function GitCommitView({ commit }: { commit: GithubCommit }) {
 
     return (
         <div className="shadow-md content-center items-center mt-3 mb-3 bg-white rounded-md p-2">
-            <p className="text-3xl text-justify text-black">Commit message: <a href={commit.html_url} target="_blank" className="hover:text-blue-700">{commit.commit.message}</a></p>
+            <p className="text-3xl text-justify text-black">Commit message: <a href={commit.html_url} target="_blank" className="hover:text-blue-700" rel="noreferrer">{commit.commit.message}</a></p>
             <div className="flex items-center text-black">
                 <p className="text-left text-2xl mr-2">Author:</p>
                 <div className="h-10 w-10 bg-cover rounded-md" style={{ backgroundImage: `url(${userInfo.avatar_url})` }} />
                 <div className="text-left text-2xl ml-2">
-                    <p><a href={userInfo.html_url} target="_blank" className="hover:text-blue-700">{userInfo.login}</a></p>
+                    <p><a href={userInfo.html_url} target="_blank" className="hover:text-blue-700" rel="noreferrer">{userInfo.login}</a></p>
                 </div>
             </div>
         </div>
