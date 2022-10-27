18bf3da3faca40e1983177c999f2ad7064114d33
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-10-27 07:18:55 -0500

    Test for Pusher Integration

---
+++ pages/api/push.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Pusher from 'pusher';
+
+export const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
+  useTLS: true,
+});
+
+export type WebhookCallbackCommit = {
+  id: string,
+  message: string,
+  timestamp: string,
+  url: string,
+  author: {
+    login: string,
+    id: number,
+    avatar_url: string,
+    html_url: string,
+    type: string,
+    site_admin: boolean
+  },
+  committer: {
+    login: string,
+    id: number,
+    avatar_url: string,
+    html_url: string,
+    type: string,
+    site_admin: boolean
+  }
+}
+
+type GithubWebhookCallbackData = {
+  commits: WebhookCallbackCommit[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data: GithubWebhookCallbackData = req.body;
+  const response = await pusher.trigger("git-watcher", "push-event", data.commits);
+  res.json({ message: "completed" });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,35 @@
 import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
-import {Octokit} from "octokit";
+import { Octokit } from "octokit";
 import GitCommitListView from "../components/GitCommitListView";
+import Pusher from "pusher-js";
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import { WebhookCallbackCommit } from './api/push';
 
 export interface GithubCommit {
-    "sha": string,
-    "commit": {
-        "author": {
-            "name": string,
-            "email": string,
-            "date": string
-        },
-        "committer": {
-            "name": string,
-            "email": string,
-            "date": string
-        },
-        "message": string
+    sha: string,
+    commit: {
+        message: string
     },
-    "author": {
-        "login": string,
-        "id": number,
-        "avatar_url": string,
-        "html_url": string,
-        "type": string,
-        "site_admin": boolean
+    author: {
+        login: string,
+        id: number,
+        avatar_url: string,
+        html_url: string,
+        type: string,
+        site_admin: boolean
     },
-    "committer": {
-        "login": string,
-        "id": number,
-        "avatar_url": string,
-        "html_url": string,
-        "type": string,
-        "site_admin": boolean
+    committer: {
+        login: string,
+        id: number,
+        avatar_url: string,
+        html_url: string,
+        type: string,
+        site_admin: boolean
     },
-    "html_url": string
+    html_url: string
 }
 
 export async function getServerSideProps() {
@@ -53,14 +47,41 @@ export async function getServerSideProps() {
     }
 }
 
-export default function Home({data}: { data: GithubCommit[] }) {
-    console.log(data);
+export default function Home({ data }: { data: GithubCommit[] }) {
+
+    const [commitHistory, setCommitHistory] = useState(data);
+
+    useEffect(() => {
+        const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+            cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+        });
+        const channel = pusher.subscribe("chat");
+
+        channel.bind("push-event", (commits: WebhookCallbackCommit[]) => {
+            setCommitHistory(previousCommitHistory => [...commits.map((commit) => {
+                return {
+                    sha: commit.id,
+                    commit: {
+                        message: commit.message
+                    },
+                    author: commit.author,
+                    committer: commit.committer,
+                    html_url: commit.url
+                }
+            }), ...previousCommitHistory])
+        });
+
+        return () => {
+            pusher.unsubscribe("push-event");
+        };
+    }, []);
+
     return (
         <div>
             <Head>
                 <title>GitWatcher</title>
-                <meta name="description" content="Watcher of commits in it's repository"/>
-                <link rel="icon" href="/gitico.png"/>
+                <meta name="description" content="Watcher of commits in it's repository" />
+                <link rel="icon" href="/gitico.png" />
             </Head>
             <main className="mt-5">
                 <h1 className={styles.title}>
@@ -72,7 +93,7 @@ export default function Home({data}: { data: GithubCommit[] }) {
                 </p>
 
                 <div className="ml-10 mr-10 mt-5">
-                    <GitCommitListView commits={data}/>
+                    <GitCommitListView commits={commitHistory} />
                 </div>
             </main>
         </div>
